15a34e8fac02d2273de3065cbac6593d31252f33
Amit Prakash <101566248+Apr28@users.noreply.github.com>
2022-05-02 12:21:33 +0530

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,8 @@
+let navigation = () => {
+    return `
+        <a href="./index.html">News App</a>
+      <input type="text" id="search_input" placeholder="Search here..">
+    `
+}
+
+export default navigation; 
